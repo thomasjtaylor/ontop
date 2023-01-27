30e56616b43b6371c90dec4288f962d10d3641ae
Thomas J. Taylor <thomas@infotechsoft.com>
2023-01-27 14:05:45 -0500

    Update Postgres for db-specific validation

--- test/rdb2rdf-compliance/src/test/java/RDB2RDFTestPostgres.java
+++ test/rdb2rdf-compliance/src/test/java/RDB2RDFTestPostgres.java
@@ -12,7 +12,7 @@ import java.util.*;
  * Test suite for RDB2RDF tests. Updated for Postgresql database.
  * <p>Hard-coded to connect to Ontop's ontop-pgsql database on port 7777. Consider using postgres:13 for better performance.
  * <p><code>docker run --name ontop_postgres_running -p 7777:5432 -e POSTGRES_PASSWORD=postgres2 -d ontop/ontop-pgsql</code>
- * 
+ * <p><code>docker run --rm --name ontop-postgres -p 7777:5432 -e POSTGRES_PASSWORD=postgres2 postgres:13</code> 
  * @author Thomas J. Taylor (mail@thomasjtaylor.com)
  */
 @RunWith(Parameterized.class)
@@ -29,35 +29,25 @@ public class RDB2RDFTestPostgres extends RDB2RDFTestBase {
 			"tc0003a",
 			// Limitation of bnode isomorphism detection + xsd:double encoding (engineering notation was expected)
 			"dg0005",
-			// Limitation of bnode isomorphism detection
-			"dg0005-modified",
 			// Different XSD.DOUBLE lexical form; was expecting the engineering notation. Modified version added.
 			"tc0005a",
-			// Modified for H2, not PSQL
-			"tc0005a-modified",
 			// Different XSD.DOUBLE lexical form; was expecting the engineering notation. Modified version added.
 			"tc0005b",
-			"tc0005b-modified",
-			// Modified (different XSD.DOUBLE lexical form)
-			"dg0012",
 			// Direct mapping and bnodes: row unique ids are not considered, leadinq to incomplete results
 			// (e.g. Bob-London should appear twice). TODO: fix it
-			"dg0012-modified",
-			// Modified (different XSD.DOUBLE lexical form)
+			"dg0012",
+			// (different XSD.DOUBLE lexical form)
 			"tc0012a",
-			"tc0012a-modified",
 			// Modified (different XSD.DOUBLE lexical form)
 			"tc0012e",
-			"tc0012e-modified",
 			// "dg0014" - ERROR: No function matches replace(integer, unknown, unknown)
-			// PSQL "varbinary" does not exist; TODO change varbinary to bytea, use decode('XXX','hex') in create.sql
+			// PSQL "varbinary" does not exist; changed varbinary to bytea, use decode('XXX','hex') in create.sql
 			"dg0016",
-			"tc0016a",
+//			"tc0016a",
 			"tc0016b",
-			"tc0016b-modified",
-			"tc0016c",
-			"tc0016d",
-			"tc0016e",
+//			"tc0016c",
+//			"tc0016d",
+//			"tc0016e",
 			// PSQL does not store the implicit trailing spaces in CHAR(15) and does not output them.
 			"dg0018",
 			// PSQL does not store the implicit trailing spaces in CHAR(15) and does not output them.
@@ -65,8 +55,14 @@ public class RDB2RDFTestPostgres extends RDB2RDFTestBase {
 			// Should create an IRI based on a column and the base IRI. TODO: support the base IRI in R2RML
 			"tc0019a"
 	);
-	private static final DbSettings dbSettings = new DbSettings("org.postgresql.Driver",
-			"jdbc:postgresql://:7777/Rdb2RdfTest", "postgres", "postgres2");
+	private static final DbSettings dbSettings = new DbSettings(
+			"pgsql",
+			"org.postgresql.Driver",
+			"jdbc:postgresql://:7777/Rdb2RdfTest", 
+			"postgres", 
+			"postgres2",
+			"Rdb2RdfTest",
+			null);
 	
 	/**
 	 * Returns the list of parameters created automatically from RDB2RDF manifest files
@@ -83,10 +79,9 @@ public class RDB2RDFTestPostgres extends RDB2RDFTestBase {
 	@BeforeClass
 	public static void createDB() {
 		try (java.sql.Connection pgConnection = DriverManager
-				.getConnection(dbSettings.url.replace("Rdb2RdfTest", "postgres"), dbSettings.user, dbSettings.password);
+				.getConnection(dbSettings.url.replace(dbSettings.database, "postgres"), dbSettings.user, dbSettings.password);
 				java.sql.Statement s = pgConnection.createStatement()) {
-			String database = "Rdb2RdfTest";
-			s.execute("CREATE DATABASE \"" + database + "\"");
+			s.execute("CREATE DATABASE \"" + dbSettings.database + "\"");
 		} catch (SQLException sqle) {
 			System.out.println(sqle);
 		}
@@ -94,17 +89,16 @@ public class RDB2RDFTestPostgres extends RDB2RDFTestBase {
 	
 	/** connect to the postgres database to drop the Rdb2RdfTest schema */
 	protected static void dropDB()  {
-        try (java.sql.Connection pgConnection = DriverManager.getConnection(dbSettings.url.replace("Rdb2RdfTest", "postgres"), dbSettings.user, dbSettings.password);
+        try (java.sql.Connection pgConnection = DriverManager.getConnection(dbSettings.url.replace(dbSettings.database, "postgres"), dbSettings.user, dbSettings.password);
                 java.sql.Statement s = pgConnection.createStatement()) {
-                String database = "Rdb2RdfTest";
                 try {
                 s.execute("SELECT pg_terminate_backend(pg_stat_activity.pid) "
                         + "FROM pg_stat_activity "
-                        + "WHERE datname = '"+database+"' "
+                        + "WHERE datname = '"+ dbSettings.database +"' "
                                 + "AND pid <> pg_backend_pid(); "
-                        + "DROP DATABASE \""+database+"\"");
+                        + "DROP DATABASE \""+ dbSettings.database +"\"");
                 } catch (SQLException sqle) {}
-                s.execute("CREATE DATABASE \""+database+"\"");
+                s.execute("CREATE DATABASE \""+ dbSettings.database +"\"");
               } catch (SQLException sqle) {
                   System.out.println(sqle);
               }
